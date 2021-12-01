1db4c391868de54303b0188141f7fea4c2ce8095
Romenig Lima Damasio <romenigld@gmail.com>
2021-12-01 23:07:38 +0100

    Refactored state and tally for a module Type

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,23 +1,13 @@
 defmodule Hangman do
   alias Hangman.Impl.Game
+  alias Hangman.Type
 
-  @type   state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
   @opaque game  :: Game.t # '@opaque' the internals of the type should remain private from anyone who imports it.
-  @type   tally :: %{
-    turns_left: integer,
-    game_state: state,
-    letters: list(String.t),
-    used: list(String.t)
-  }
 
   @spec new_game() :: game
   defdelegate new_game, to: Game
-  # defdelegate is the same as the code underneath
-  # def new_game do
-  #   Game.new_game
-  # end
 
-  @spec make_move(game, String.t) :: {game, tally}
+  @spec make_move(game, String.t) :: {game, Type.tally}
   def make_move(_game, _guess) do
   end
 end
