f6639805adea028b66cdf57f7d937c376fcd85b5
Romenig Lima Damasio <romenigld@gmail.com>
2021-12-23 17:27:19 +0100

    And a Bad Guess

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -56,7 +56,7 @@ defmodule Hangman.Impl.Game do
     %{ game | game_state: new_state}
   end
   def score_guess(game = %{ turns_left: 1 }, _bad_guess) do
-    %{ game | game_state: :lost }
+    %{ game | game_state: :lost, turns_left: 0 }
   end
   def score_guess(game, _bad_guess) do
     # turns_left == 1 -> lost | dec turns_left, :bad_guess
@@ -68,7 +68,7 @@ defmodule Hangman.Impl.Game do
     %{
       turns_left: game.turns_left,
       game_state: game.game_state,
-      letters: [],
+      letters: reveal_guessed_letters(game),
       used: game.used |> MapSet.to_list |> Enum.sort
     }
   end
@@ -79,4 +79,12 @@ defmodule Hangman.Impl.Game do
 
   defp maybe_won(true), do: :won
   defp maybe_won(_),    do: :good_guess
+
+  defp reveal_guessed_letters(game) do
+    game.letters
+    |> Enum.map(fn letter -> MapSet.member?(game.used, letter) |> maybe_reveal(letter) end)
+  end
+
+  defp maybe_reveal(true, letter), do: letter
+  defp maybe_reveal(_, _letter), do: "_"
 end
