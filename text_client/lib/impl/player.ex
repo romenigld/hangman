f54d74406cc57128d15decdf8ed933a0324c1547
Romenig Lima Damasio <romenigld@gmail.com>
2021-12-30 20:26:54 +0100

    colorized the game

--- text_client/lib/impl/player.ex
+++ text_client/lib/impl/player.ex
@@ -13,41 +13,50 @@ defmodule TextClient.Impl.Player do
   @spec interact(state) :: :ok
 
   def interact({_game, tally = %{ game_state: :won}}) do
-    IO.puts ["Congratulations. You won! The word is \'",
-      IO.ANSI.format([:green_background,"#{tally.letters}"]),
-      "\'"]
+    IO.puts [
+      colorize(:blink_slow, colorize(:yellow, "Congratulations. You won! \nThe word is \'")),
+      colorize(:green_background,"#{tally.letters}"),
+      colorize(:yellow, "\'.")
+    ]
   end
 
   def interact({_game, tally = %{ game_state: :lost}}) do
-    IO.puts ["Sorry, you lost... the word was \'",
-     IO.ANSI.format([:red_background,"#{tally.letters |> Enum.join}"]),
-     "\'"]
+    IO.puts [
+      colorize(:blink_slow, colorize(:red, "Sorry, you lost...")),
+      colorize(:red, "\nThe correct word was \'"),
+      colorize(:red_background,"#{tally.letters |> Enum.join}"),
+      colorize(:red, "\'.")
+    ]
   end
 
   def interact({ game, tally }) do
-    IO.puts feedback_for(tally)
+    IO.puts [feedback_for(tally)]
     IO.puts current_word(tally)
     Hangman.make_move(game, get_guess())
     |> interact()
   end
 
   def get_guess() do
-    IO.gets("Next letter: ")
+    IO.gets(colorize(:green, "Next letter: "))
     |> String.trim()
     |> String.downcase()
   end
 
   def current_word(tally) do
    [
-    "Word so far: ", tally.letters |> Enum.join(" "),
-    IO.ANSI.format([:green, "   (turns left: "]), IO.ANSI.format([:blue, tally.turns_left |> to_string]),
-    IO.ANSI.format([:green,"   used so far: "]), IO.ANSI.format([:yellow, tally.used |> Enum.join(",")]),
-    IO.ANSI.format([:green, ")"])
+    colorize(:green, "Word so far: "), tally.letters |> Enum.join(" "),
+    colorize(:green, "   (turns left: "), colorize(:blink_slow, colorize(:red_background, tally.turns_left |> to_string)),
+    colorize(:green,"   used so far: "), colorize(:yellow, tally.used |> Enum.join(",")),
+    colorize(:green, ")")
    ]
   end
 
-  def feedback_for(tally  = %{ game_state: :initializing }), do: "Welcome! I'm thinking of a #{tally.letters |> length} letter word"
-  def feedback_for(_tally = %{ game_state: :good_guess }),   do: "Good guess!"
-  def feedback_for(_tally = %{ game_state: :bad_guess }),    do: "Sorry, that letter's not in the word"
-  def feedback_for(_tally = %{ game_state: :already_used }), do: "You already used that letter"
+  def feedback_for(tally  = %{ game_state: :initializing }), do: [colorize(:blue, "Welcome! \nI'm thinking of a "), colorize(:yellow_background, "#{tally.letters |> length}"), colorize(:blue, " letter word.")]
+  def feedback_for(_tally = %{ game_state: :good_guess }),   do: colorize(:cyan, "Good guess!")
+  def feedback_for(_tally = %{ game_state: :bad_guess }),    do: colorize(:red, "Sorry, that letter's not in the word.")
+  def feedback_for(_tally = %{ game_state: :already_used }), do: colorize(:magenta, "You already used that letter.")
+
+  def colorize(type_color, string) do
+    IO.ANSI.format([type_color, string])
+  end
 end
