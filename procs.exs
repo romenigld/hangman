49d010c85707aff281b43a8de4593c539dda2b3b
Romenig Lima Damasio <romenigld@gmail.com>
2022-01-17 13:46:19 +0100

    Pattern Matching Messages

--- procs.exs
+++ procs.exs
@@ -1,9 +1,15 @@
 defmodule Procs do
-  def hello() do
+  def hello(count) do
     receive do
+      {:quit} ->
+        IO.puts "I'm outta here"
+
+      {:add, n} ->
+        hello(count + n)
+
       msg ->
-        IO.puts("Hello #{inspect msg}")
+        IO.puts("#{count}: Hello #{inspect msg}")
+        hello(count)
     end
-    hello()
   end
 end
