a46a08b4012933f3f9fa93ce4ac441511de49f42
Jorge Bejar <jorge@wyeworks.com>
2022-01-19 14:31:48 -0300

    Elixir forum thread

--- dictionary/lib/runtime/application.ex
+++ dictionary/lib/runtime/application.ex
@@ -3,6 +3,8 @@ defmodule Dictionary.Runtime.Application do
   use Application
 
   def start(_type, _args) do
-    Dictionary.Runtime.Server.start_link()
+    # We are forced by design to pass an initial value here
+    # You may want to read the file and pass the data here
+    Dictionary.Runtime.Server.start_link([])
   end
 end
