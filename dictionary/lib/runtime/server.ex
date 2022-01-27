07844bc6cdf5df33c8f07c99f37119953bef0548
Romenig Lima Damasio <romenigld@gmail.com>
2022-01-27 19:13:21 +0100

    fixed the erlang :random to :rand function

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -12,7 +12,7 @@ defmodule Dictionary.Runtime.Server do
   end
 
   def random_word() do
-    if :random.uniform < 0.33 do
+    if :rand.uniform < 0.33 do
       Agent.get(@me, fn _ -> exit(:boom) end)
     end
 
