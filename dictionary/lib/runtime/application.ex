c4e9438383b7c1efac7e020952990adc8c78e97f
Romenig Lima Damasio <romenigld@gmail.com>
2022-01-28 12:09:45 +0100

    commented the default max_restarts and max_seconds(default to 1 and 5) of the Supervisor options

--- dictionary/lib/runtime/application.ex
+++ dictionary/lib/runtime/application.ex
@@ -7,7 +7,10 @@ defmodule Dictionary.Runtime.Application do
 
     options = [
       name: Dictionary.Runtime.Supervisor,
-      strategy: :one_for_one
+      strategy: :one_for_one,
+      # defaults to 1 and 5
+      # max_restarts: 2,
+      # max_seconds: 9,
     ]
 
     Supervisor.start_link(children, options)
