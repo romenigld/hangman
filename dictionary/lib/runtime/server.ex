92459f94b03f27bb4b29f46254d29b6d900a974a
Romenig Lima Damasio <romenigld@gmail.com>
2022-02-14 13:50:27 +0100

    comment the kill Agent process randomly

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -12,9 +12,10 @@ defmodule Dictionary.Runtime.Server do
   end
 
   def random_word() do
-    if :rand.uniform < 0.33 do
-      Agent.get(@me, fn _ -> exit(:boom) end)
-    end
+    # it will kill the process if the random number in erlang is less than 0.33
+    # if :rand.uniform < 0.33 do
+    #   Agent.get(@me, fn _ -> exit(:boom) end)
+    # end
 
     Agent.get(@me, &WordList.random_word/1)
   end
