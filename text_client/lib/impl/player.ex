22c00bfdfc1c56406ce12ffd57e0e275fabeead2
Romenig Lima Damasio <romenigld@gmail.com>
2022-02-14 19:48:25 +0100

    Creating Remote Servers

--- text_client/lib/impl/player.ex
+++ text_client/lib/impl/player.ex
@@ -3,15 +3,13 @@ defmodule TextClient.Impl.Player do
   @typep tally :: Hangman.tally
   @typep state :: { game, tally }
 
-  @spec start() :: :ok
-  def start do
-    game = Hangman.new_game()
+  @spec start(game) :: :ok
+  def start(game) do
     tally = Hangman.tally(game)
     interact({ game, tally })
   end
 
   @spec interact(state) :: :ok
-
   def interact({_game, tally = %{ game_state: :won}}) do
     IO.puts [
       colorize(:blink_slow, colorize(:yellow, "Congratulations. You won!")),
