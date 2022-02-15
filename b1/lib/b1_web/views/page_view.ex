b986b2544e0daef9e2966e7ecbcb07c062f5eb9a
Romenig Lima Damasio <romenigld@gmail.com>
2022-02-15 13:14:13 +0100

    Playing with Phoenix: Views and Templates - Your Turn

--- b1/lib/b1_web/views/page_view.ex
+++ b1/lib/b1_web/views/page_view.ex
@@ -1,6 +1,12 @@
 defmodule B1Web.PageView do
   use B1Web, :view
 
-  def plural_phrase(1, noun), do: "one #{noun}"
+  def plural_phrase(1, noun), do: "1 #{noun}"
+  def plural_phrase(n, noun) when (n < 0) do
+    "<span style='color: red'>
+      #{n} #{noun}
+    </span>"
+    |> raw()
+  end
   def plural_phrase(n, noun), do: "#{n} #{noun}s"
 end
